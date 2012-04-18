4b03d48a8c702f8adc4eb1acb9b3a6dc3d227492
Som Mood <sommood55@gmail.com>
2012-04-18 13:13:45 +0700

    First Commit
    Just Empty Application

---
+++ TestGitHub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestGitHub
+//
+//  Created by boonyanit.m on 4/18/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestGitHub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestGitHub
+//
+//  Created by boonyanit.m on 4/18/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
